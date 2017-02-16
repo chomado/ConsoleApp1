ff2fa49c066f81d3ecc19e27be8dd03307cb01dd
Madoka Chiyoda <chomado@gmail.com>
2017-02-16 16:31:17 +0900

    dynamic で受け取っていた json のデータを、ちゃんと型で受け取るようにした。（VSの Json からクラスを自動生成する機能を使った。型で受け取ることにより、インテリセンスも効くよ）

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleApp1
         private static async Task WeatherAsync()
         {
             var weatherApi = "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040";
-            
+
             // .NET で HTTP を扱うには、HttpClient というクラスを使う
             var client = new HttpClient();
 
@@ -36,8 +36,98 @@ namespace ConsoleApp1
 
             // Json.NET で、JSON をデシリアライズします。
             // ここで、C#の機能のひとつである dynamic という、型の無い動的なオブジェクトとして受け取ります
-            dynamic result = JsonConvert.DeserializeObject(json);
+            var result = JsonConvert.DeserializeObject<Rootobject>(json);
             Console.WriteLine(result.description.text);
         }
     }
+
+    public class Rootobject
+    {
+        public Pinpointlocation[] pinpointLocations { get; set; }
+        public string link { get; set; }
+        public Forecast[] forecasts { get; set; }
+        public Location location { get; set; }
+        public DateTime publicTime { get; set; }
+        public Copyright copyright { get; set; }
+        public string title { get; set; }
+        public Description description { get; set; }
+    }
+
+    public class Location
+    {
+        public string city { get; set; }
+        public string area { get; set; }
+        public string prefecture { get; set; }
+    }
+
+    public class Copyright
+    {
+        public Provider[] provider { get; set; }
+        public string link { get; set; }
+        public string title { get; set; }
+        public Image image { get; set; }
+    }
+
+    public class Image
+    {
+        public int width { get; set; }
+        public string link { get; set; }
+        public string url { get; set; }
+        public string title { get; set; }
+        public int height { get; set; }
+    }
+
+    public class Provider
+    {
+        public string link { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Description
+    {
+        public string text { get; set; }
+        public DateTime publicTime { get; set; }
+    }
+
+    public class Pinpointlocation
+    {
+        public string link { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Forecast
+    {
+        public string dateLabel { get; set; }
+        public string telop { get; set; }
+        public string date { get; set; }
+        public Temperature temperature { get; set; }
+        public Image1 image { get; set; }
+    }
+
+    public class Temperature
+    {
+        public Min min { get; set; }
+        public Max max { get; set; }
+    }
+
+    public class Min
+    {
+        public string celsius { get; set; }
+        public string fahrenheit { get; set; }
+    }
+
+    public class Max
+    {
+        public string celsius { get; set; }
+        public string fahrenheit { get; set; }
+    }
+
+    public class Image1
+    {
+        public int width { get; set; }
+        public string url { get; set; }
+        public string title { get; set; }
+        public int height { get; set; }
+    }
+
 }
