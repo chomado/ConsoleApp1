c21d6c4fb84a7348fe1b360fd8e1eabf5eec5c6b
Madoka Chiyoda <chomado@gmail.com>
2017-02-16 16:13:39 +0900

    取得した生のJSONデータがコンソールに出力される

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            WeatherAsync().Wait();
+            Console.ReadKey(); // 何かキーを押すまでプログラムが終了しない
+        }
+
+        private static async Task WeatherAsync()
+        {
+            var weatherApi = "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040";
+            
+            // .NET で HTTP を扱うには、HttpClient というクラスを使う
+            var client = new HttpClient();
+
+
+            // Webからデータを「非同期で」取ってくる
+            // (C# では、他の言語のような「コールバック」を使わず、await と書くことで、非同期処理の完了を待つことができる)
+            var response = await client.GetAsync(weatherApi);
+
+            // レスポンスから body のテキストを読み取る
+            var json = await response.Content.ReadAsStringAsync();
+
+            // 標準出力に表示。取得した生のJSONデータがコンソールに出力される
+            Console.WriteLine(json);
         }
     }
 }
